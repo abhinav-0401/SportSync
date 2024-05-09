4038b81bbbcf5f8faf01933149ee5595fa0ab54e
Abhinav Jha <linuxlenin04@gmail.com>
2024-05-09 10:00:49 +0530

    layout of bonanza page

---
+++ app/cricket/page.tsx
@@ -0,0 +1,7 @@
+export default function Cricket() {
+  return (
+    <div className="bg-[#E6E6DD] px-20 py-16 gap-20 flex flex-col h-[100vh]">
+      <input type="search" name="" id="" />
+    </div>
+  );
+}
