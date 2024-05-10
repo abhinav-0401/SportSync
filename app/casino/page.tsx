d6b50c08c834e0bfba36f17f6637a1ee75a54545
Abhinav Jha <linuxlenin04@gmail.com>
2024-05-10 20:54:29 +0530

    fixed issues and added analytics page

--- app/casino/page.tsx
+++ app/casino/page.tsx
@@ -11,14 +11,14 @@ export default function Casino() {
     <div className="bg-[#E6E6DD] min-h-screen">
       <div className="flex flex-col gap-20 px-4 py-4 sm:px-10 sm:py-8 lg:px-20 lg:py-16">
         
-        <div className="px-32 flex flex-col gap-20">
+        <div className="px-8 sm:px-16 md:px-24 lg:px-32 flex flex-col gap-20">
             <div className="flex justify-center mt-4 font-bold text-4xl">
               <h1>CASINO</h1>
             </div>
 
             <div className="flex bg-white/40 px-1 py-1 rounded-full">
               <Image src="/search.png" className="ml-4 object-contain" alt="search" width={18} height={18} />
-              <Input type="search" className="rounded-full mx-4 border-0 focus-visible:ring-0 bg-transparent" />
+            <Input type="search" placeholder="Indian Premier League" className="rounded-full mx-4 border-0 focus-visible:ring-0 bg-transparent" />
               <Button className="rounded-full">Search</Button>
             </div>
         </div>
@@ -27,17 +27,17 @@ export default function Casino() {
           <Tabs defaultValue="all" className="w-full">
             <TabsList className="flex w-full justify-between bg-transparent">
               <TabsTrigger className="flex-grow" variant={"outline"} value="all">All Casino</TabsTrigger>
-              <TabsTrigger className="flex-grow" variant={"outline"} value="cricket">Cricket</TabsTrigger>
-              <TabsTrigger className="flex-grow" variant={"outline"} value="football">Football</TabsTrigger>
+              <TabsTrigger className="flex-grow" variant={"outline"} value="top">Top Casinos</TabsTrigger>
+              <TabsTrigger className="flex-grow" variant={"outline"} value="hot">Hot Casinos</TabsTrigger>
             </TabsList>
             <TabsContent value="all" className="flex w-full flex-col px-12 gap-10">
               <CasinoList type="all" />
             </TabsContent>
-            <TabsContent value="cricket" className="flex w-full flex-col px-12 gap-10">
-              <CasinoList type="cricket" />
+            <TabsContent value="top" className="flex w-full flex-col px-12 gap-10">
+              <CasinoList type="top" />
             </TabsContent>
-            <TabsContent value="football" className="flex w-full flex-col px-12 gap-10">
-              <CasinoList type="football" />
+            <TabsContent value="hot" className="flex w-full flex-col px-12 gap-10">
+              <CasinoList type="hot" />
             </TabsContent>
           </Tabs>
         </div>
---
+++ components/MatchStats.tsx
@@ -0,0 +1,59 @@
+import Image from "next/image";
+
+export default function MatchStats() {
+  return (
+    <div className="pt-4 sm:pt-8 md:pt-16 lg:pt-24 flex flex-col gap-12">
+
+      <div className="flex flex-col items-start gap-8">
+
+        <div className="bg-black text-white rounded-full py-1 px-2 sm:px-3 md:ml-8">Live</div>
+
+      </div>
+
+      <div className="flex justify-between px-4 sm:px-8 md:px-16">
+        <div className="flex gap-2">
+          {/* img + country name */}
+          <div className="flex flex-col gap-2 md:gap-4">
+            <Image src="/india.png" alt='india' width={36} height={24} />
+            <div className="font-semibold text-base md:text-lg">IND</div>
+          </div>
+          <div className="font-medium text-base md:text-lg">198/3</div>
+        </div>
+        <div className="flex gap-2">
+          {/* img + country name */}
+          <div className="font-medium text-base md:text-lg">YET TO BAT</div>
+          <div className="flex flex-col gap-2 md:gap-4">
+            <Image src="/england.png" alt='india' width={36} height={24} />
+            <div className="font-semibold text-base md:text-lg">ENG</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="text-center font-medium text-lg">IND chose to bat</div>
+
+      {/* scores */}
+      <div className="flex md:flex-row flex-col gap-8 md:gap-0 justify-between px-4 sm:px-8 md:px-16">
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2 font-semibold text-sm md:text-base">
+            <span>KL Rahul</span>
+            <span>20* (19)</span>
+          </div>
+          <div className="flex gap-2 font-normal text-sm md:text-base">
+            <span>MS DHONI</span>
+            <span>56* (50)</span>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2 md:text-base text-sm">
+            <span>20* (19)</span>
+            <span>KL Rahul</span>
+          </div>
+          <div className="flex gap-2 text-sm md:text-base">
+            <span>56* (50)</span>
+            <span>MS DHONI</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
