86fa13b713a3c24864a4820c29552b072f407bf0
Abhinav Jha <linuxlenin04@gmail.com>
2024-06-05 19:17:48 +0530

    design issues and read more implemented in casino cards and hot topics

--- components/CasinoList.tsx
+++ components/CasinoList.tsx
@@ -73,7 +73,7 @@ export default function CasinoList({ type }: Props) {
   ];
 
   return (
-    <div className="flex md:flex-row flex-col items-center md:items-start flex-wrap justify-between md:justify-normal md:gap-4 lg:gap-8">
+    <div className="flex md:flex-row flex-col items-center md:items-start flex-wrap justify-between md:justify-normal gap-4 lg:gap-8">
       {casinoData?.map((casino: any, index: number) => {
         if (casino?.data?.type == type || type == "all") {
           return <CasinoCard title={casino?.data?.title} key={index} description={casino?.data?.content} imageUrl={casino?.data?.imageUrl ?? "/article-1.png"} redirectUrl={casino?.data?.redirectUrl} />;
--- components/ui/articleCard.tsx
+++ components/ui/articleCard.tsx
@@ -13,7 +13,7 @@ interface ArticleCardProps {
 
 const ArticleCard: React.FC<ArticleCardProps> = ({ title, description, date, imageUrl }) => {
     return (
-        <div className="flex md:flex-row max-w-[320px] md:max-w-fit lg:min-w-full flex-col gap-5 p-5 hover:bg-[#45474a80] cursor-pointer bg-white/66 rounded-xl border border-solid border-gray-300/66 shadow-lg">
+        <div className="flex hover:cursor-pointer hover:bg-white/50 dark:hover:bg-[#45474A66]/40 md:flex-row max-w-[320px] md:max-w-fit lg:min-w-full flex-col gap-5 p-5 cursor-pointer bg-white/66 rounded-xl border border-solid border-gray-300/66 shadow-lg">
             <div className="flex md:min-w-fit">
                 <Image src={imageUrl} className='md:min-h-full md:max-w-full' alt={title} width={300} height={200} />
             </div>
