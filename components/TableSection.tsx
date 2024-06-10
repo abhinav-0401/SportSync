3af7fe4dd9f8948adb9e227dca7666c54681b4a8
Abhinav Jha <linuxlenin04@gmail.com>
2024-06-10 12:27:36 +0530

    fixed multiple groups not showing up and zero losses and wins not showing up in points table for cricket

--- components/TableSection.tsx
+++ components/TableSection.tsx
@@ -19,60 +19,46 @@ type Props = {
 }
 
 function TableSection({ seriesName, pointsTable }: Props) {
+  console.log(pointsTable);
+
   return (
     <div className='flex flex-col items-start justify-center space-y-7'>
       <span className='font-medium text-lg'>{seriesName}</span>
-      {/* <Tabs defaultValue='IND' className='w-full'>
-        <TabsList className='bg-transparent w-full'>
-          <TabsTrigger className="flex-grow" variant={"outline"} value="IND">IND</TabsTrigger>
-          <TabsTrigger className="flex-grow" variant={"outline"} value="ENG">ENG</TabsTrigger>
-        </TabsList>
-        <TabsContent value='IND' className='flex flex-col items-center justify-center w-full space-y-7'>
-          <div className='flex items-center px-0 justify-between w-full'>
-            <div className="flex gap-2 md:gap-4">
-              <Image src="/india.png" alt='india' width={36} height={24} className='rounded-lg' />
-              <div className="font-semibold text-sm sm:text-base md:text-lg">IND</div>
-            </div>
-            <span className='font-semibold text-sm sm:text-base md:text-lg'>Bowling</span>
-            <span className='font-semibold text-sm sm:text-base md:text-lg'>96-1 (10.1ov)</span>
-          </div>
-          <CustomTable />
-          
-
-        </TabsContent>
-        <TabsContent value='ENG'>
-
-        </TabsContent>
-      </Tabs> */}
-      <Table className='mb-12'>
-        {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
-        <TableHeader>
-          <TableRow>
-            <TableHead className="">Team</TableHead>
-            <TableHead>M</TableHead>
-            <TableHead>W</TableHead>
-            <TableHead>L</TableHead>
-            <TableHead>NRR</TableHead>
-            <TableHead className="text-right">Pts</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {
-            pointsTable?.pointsTable &&  pointsTable?.pointsTable[0]?.pointsTableInfo?.map((row: any, index: any) => {
-              return (
-                <TableRow key={index}>
-                  <TableCell className="font-medium">{row?.teamFullName}</TableCell>
-                  <TableCell>{row?.matchesPlayed}</TableCell>
-                  <TableCell>{row?.matchesWon}</TableCell>
-                  <TableCell className="">{row?.matchesLost}</TableCell>
-                  <TableCell className="">{row?.nrr}</TableCell>
-                  <TableCell className="text-right">{row?.points}</TableCell>
+      {
+        pointsTable?.pointsTable && pointsTable?.pointsTable?.map((group: any, index: number) => {
+          return (
+            <Table className='mb-12'>
+              <TableCaption>{group?.groupName}</TableCaption>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="">Team</TableHead>
+                  <TableHead>M</TableHead>
+                  <TableHead>W</TableHead>
+                  <TableHead>L</TableHead>
+                  <TableHead>NRR</TableHead>
+                  <TableHead className="text-right">Pts</TableHead>
                 </TableRow>
-              );
-            })
-          }
-        </TableBody>
-      </Table>
+              </TableHeader>
+              <TableBody>
+                {
+                  group?.pointsTableInfo?.map((row: any, index: any) => {
+                    return (
+                      <TableRow key={index}>
+                        <TableCell className="font-medium">{row?.teamFullName}</TableCell>
+                        <TableCell>{row?.matchesPlayed}</TableCell>
+                        <TableCell>{row?.matchesWon ?? 0}</TableCell>
+                        <TableCell className="">{row?.matchesLost ?? 0}</TableCell>
+                        <TableCell className="">{row?.nrr}</TableCell>
+                        <TableCell className="text-right">{row?.points ?? 0}</TableCell>
+                      </TableRow>
+                    );
+                  })
+                }
+              </TableBody>
+            </Table>
+          );
+        })
+      }
     </div>
   )
 }
